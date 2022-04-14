e55093efdc6948c3ce93d5c89e78755f2bed47ad
BHUVAN-RJ <83565908+BHUVAN-RJ@users.noreply.github.com>
2022-04-14 12:21:34 +0530

    Add files via upload

---
+++ proj-class_demo/main.py
@@ -0,0 +1,25 @@
+# class Student:
+#     def __init__(self, name, usn, location):
+#         self.Name = name
+#         self.USN = usn
+#         self.Loc = location
+#         self.followers = 0
+#         self.following = 0
+#
+#     def follow(self, user):
+#         user.followers += 1
+#         self.following += 1
+#
+#
+# stu_1 = Student("Bhuvan", 1, "Hassan")
+# stu_2 = Student("gagan",2,"hassan")
+# print(stu_1.USN)
+# stu_1.follow(stu_2)
+# print(stu_1.following)
+# print(stu_2.following)
+# print(stu_1.followers)
+# print(stu_2.followers)
+h = 10
+s = 9
+srt = f"hus{10}" + f"_{s}"
+print(srt)
